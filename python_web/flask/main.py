91757c52eab02cda247a03bab40e0817c498499c
Cyberbolt <735245473@qq.com>
2022-11-24 13:09:58 +0000

    新增 flask 测试

--- python_web/flask/main.py
+++ python_web/flask/main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello, World!</p>'
+    return jsonify({
+        'code': 1
+    })
